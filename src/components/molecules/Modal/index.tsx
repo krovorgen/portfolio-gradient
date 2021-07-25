1f481566a50dd91c2373c42ab865333a8f30d739
Максим Сергеевич <krovorgen@gmail.com>
2021-07-26 00:01:38 +0300

    optimized picture

--- src/components/molecules/Modal/index.tsx
+++ src/components/molecules/Modal/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC, useEffect, useRef } from 'react';
 
 import styles from './style.module.scss';
+import classNames from 'classnames';
 
 interface IModalProps {
   callBack: () => void;
+  variant?: 'with-item';
 }
 
-const Modal: FC<IModalProps> = ({ children, callBack }) => {
+const Modal: FC<IModalProps> = ({ children, callBack, variant }) => {
   const boxRef = useRef<HTMLDivElement>(null);
   const handleOutsideClick = (e: any) => {
     if (!e.path.includes(boxRef.current)) {
@@ -20,9 +22,13 @@ const Modal: FC<IModalProps> = ({ children, callBack }) => {
     };
   }, []);
 
+  const appearances = {
+    [styles['modal__box--with-item']]: variant === 'with-item',
+  };
+
   return (
     <div className={styles['modal']}>
-      <div className={styles['modal__box']} ref={boxRef}>
+      <div className={classNames(styles['modal__box'], appearances)} ref={boxRef}>
         {children}
         <button className={styles['modal__close']} onClick={callBack} />
       </div>
--- src/components/molecules/ModalWork/index.tsx
+++ src/components/molecules/ModalWork/index.tsx
@@ -4,6 +4,7 @@ import { Modal } from '@/components/index';
 import { SliderContentType } from '../../../types';
 
 import styles from './style.module.scss';
+import Image from 'next/image';
 
 interface IModalWorkProps {
   count: number;
@@ -24,7 +25,30 @@ const ModalWork: FC<IModalWorkProps> = ({ count, sliderMockData }) => {
           Смотреть все ({count})
         </button>
       </div>
-      {modalStatus && <Modal callBack={toggleModalStatus}>{JSON.stringify(sliderMockData)}</Modal>}
+      {modalStatus && (
+        <Modal callBack={toggleModalStatus} variant={'with-item'}>
+          <ul className={styles['modal-work__items']}>
+            {sliderMockData.map((item, index) => (
+              <li className={styles['modal-work__item']} key={index}>
+                <a
+                  className={styles['modal-work__link']}
+                  href={item.link}
+                  target={'_blank'}
+                  rel={'noreferrer'}
+                >
+                  <Image
+                    className={styles['modal-work__preview']}
+                    src={item.img}
+                    width={282}
+                    height={165}
+                    alt={'Slider image'}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Modal>
+      )}
     </div>
   );
 };
