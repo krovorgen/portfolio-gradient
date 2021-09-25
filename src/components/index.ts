8fbb6d5cd85180fa85b6b4908cfe6ac558b25738
Maksim Sergeevich <krovorgen@gmail.com>
2021-09-25 14:09:54 +0300

    Reorder main components

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-// organisms
-export { default as Header } from './organisms/Header';
-export { default as Hero } from './organisms/Hero';
-export { default as About } from './organisms/About';
-export { default as Skills } from './organisms/Skills';
-export { default as Slider } from './organisms/Slider';
-export { default as Cooperation } from './organisms/Cooperation';
-export { default as Footer } from './organisms/Footer';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import { About, Footer, Header, Hero, Skills, Slider } from '@/components/index';
-import Cooperation from '@/components/organisms/Cooperation';
+import React from 'react';
+
+import { About } from '@/components/organisms/About';
+import { Cooperation } from '@/components/organisms/Cooperation';
+import { Footer } from '@/components/organisms/Footer';
+import { Header } from '@/components/organisms/Header';
+import { Hero } from '@/components/organisms/Hero';
+import { Skills } from '@/components/organisms/Skills';
+import { Slider } from '@/components/organisms/Slider';
 import { creativeMockData } from 'data/creativeMockData';
 import { portfolioMockData } from 'data/portfolioMockData';
 
